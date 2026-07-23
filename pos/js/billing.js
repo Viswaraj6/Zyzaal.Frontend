a597ebe3086ed23c70b87b4a3e92e910532a2743
Viswaraj6 <75193269+Viswaraj6@users.noreply.github.com>
2026-07-23 18:01:48 +0530

    Update billing.js

--- pos/js/billing.js
+++ pos/js/billing.js
@@ -1,1 +1,11 @@
-console.log("ZYZAAL POS");
+const cards = document.querySelectorAll(".product-card");
+
+cards.forEach(card => {
+
+    card.addEventListener("click", () => {
+
+        alert("Product Added");
+
+    });
+
+});
