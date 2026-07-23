8d2efe61b7c0e4bbd925b0b70d14aad1f9a6f42f
Viswaraj6 <75193269+Viswaraj6@users.noreply.github.com>
2026-07-24 03:16:28 +0530

    Update billing.js

--- pos/js/billing.js
+++ pos/js/billing.js
@@ -481,8 +481,10 @@ function showScanToast(text){
 }
 function toggleSidebar(){
 
-    document
-        .getElementById("sidebar")
-        .classList.toggle("open");
+    const sidebar = document.getElementById("sidebar");
+    const overlay = document.getElementById("sidebarOverlay");
+
+    sidebar.classList.toggle("open");
+    overlay.classList.toggle("show");
 
 }
